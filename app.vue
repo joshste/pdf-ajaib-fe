125b8d898ca1be29df262324c70e5b1169d4f09a
joshste <josh.steven@ui.ac.id>
2022-11-27 17:44:27 +0700

    Test cd

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="h-screen flex justify-center items-center">
-      <p class="text-[#b07171]">Hello world!</p>
+      <p class="text-[#b07171]">Hello world!2</p>
     </div>
   </div>
 </template>
