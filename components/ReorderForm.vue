9f6437a4d7975d40aafb934d5fc352de199c1193
joshste <josh.steven@ui.ac.id>
2022-12-03 09:04:53 +0700

    Use ts lang

--- components/ReorderForm.vue
+++ components/ReorderForm.vue
@@ -4,45 +4,48 @@
     <input type="number" name="reorder" v-model="indexB" @input="onInputChange" />
     <button @click="onSubmit">Submit</button>
     <a :href="pdfUrl" v-if="downloaded" download="result.pdf">Download</a>
+
+    <ErrorNotification :activated="isNotificationVisible" @onClose="isNotificationVisible = false"/>
 </template>
 
-<script setup>
-const readyPdf = ref(false);
-const readyInput = ref(false);
-const file = ref();
+<script setup lang="ts">
+let readyInput = false;
+let file: File | undefined;
+const isNotificationVisible = ref(false);
 const indexA = ref();
 const indexB = ref();
 const pdfUrl = ref("");
 const downloaded = ref(false);
 
 
-const updateFile = (pdf) => {
+const updateFile = (pdf: File | undefined) => {
     downloaded.value = false;
-    readyPdf.value = false;
-    if (pdf) {
-        file.value = pdf;
-        readyPdf.value = true;
-    }
+    file = pdf;
 }
 
-const onInputChange = (e) => {
+const onInputChange = () => {
     downloaded.value = false;
-    readyInput.value = (indexA.value && indexB.value) ? true : false;
+    readyInput = (indexA.value !== "" && indexB.value !== "") ? true : false;
 }
 
-const onSubmit = async (e) => {
-    if (!readyPdf.value || !readyInput.value) return;
-    
+const onSubmit = async () => {
+    if (!readyInput || !file) return;
+
     const formData = new FormData();
-    formData.append("swap-instruction", [indexA.value, indexB.value])
-    formData.append("pdf-file", file.value);
-    const response = await customFetch("reorder-pdf", {
-        method: "post",
-        body: formData
-    });
+    formData.append("swap-instruction", `${indexA.value},${indexB.value}`)
+    formData.append("pdf-file", file);
+    try {
+        const response = await customFetch<Blob>("reorder-pdf", {
+            method: "post",
+            body: formData
+        });
+        pdfUrl.value = window.URL.createObjectURL(response);
+        downloaded.value = true;
+    }
+    catch (e) {
+        isNotificationVisible.value = true;
+    }
 
-    pdfUrl.value = window.URL.createObjectURL(response);
-    downloaded.value = true;
 }
 </script>
 
