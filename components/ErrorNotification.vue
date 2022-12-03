3ab754af604a14cadef9c7afab4a709692b45393
joshste <josh.steven@ui.ac.id>
2022-12-03 09:05:05 +0700

    Add model to show when error

---
+++ components/ErrorNotification.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="fixed w-screen h-screen top-0 bottom-0 left-0 right-0" v-if="activated" @click="onClose">
+        <div class ="flex h-screen w-screen justify-center items-center">
+          <h1> Please Try Again </h1>
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  activated: boolean 
+}>();
+
+const emit = defineEmits<{
+  (e: "onClose"): void; 
+}>();
+
+const onClose = () => {
+  emit("onClose");
+}
+</script>
