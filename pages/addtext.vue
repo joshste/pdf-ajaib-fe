8addff378fc35db3c68b6a4353a374524a5906d5
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2022-12-10 13:59:49 +0700

    Add addtext feature

---
+++ pages/addtext.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex max-w-[100vw]">
+        <ReorderForm v-model:file="file" />
+        <EmbedPDFView :pdfBlob="blob" />
+    </div>
+</template>
+
+<script setup lang="ts">
+let file = ref<File | undefined>();
+let blob = computed(() => {
+    if (!file.value) {
+        return undefined;
+    }
+    return new Blob([file.value], { type: "application/pdf" });
+})
+watch(file, (file) => {
+    console.log(`watched: ${file}`);
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
         <div class="h-screen flex justify-center items-center">
             <NuxtLink to="/reorder">Reorder PDF</NuxtLink>
         </div>
+        <div class="h-screen flex justify-center items-center">
+          <NuxtLink to="/addtext">Add Text</NuxtLink>
+        </div>
     </div>
 </template>
   
