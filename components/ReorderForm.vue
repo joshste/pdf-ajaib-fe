62a83114c49af3b147e28ca590382e5f67144f14
joshste <josh.steven@ui.ac.id>
2022-12-10 16:35:28 +0700

    adjust style

--- components/ReorderForm.vue
+++ components/ReorderForm.vue
@@ -1,10 +1,32 @@
 <template>
     <div class="flex flex-1 flex-col justify-center items-center">
-        <UploadPDFButton @pdfChanged="updateFile" />
-        <input type="number" name="reorder" v-model="indexA" @input="onInputChange" />
-        <input type="number" name="reorder" v-model="indexB" @input="onInputChange" />
-        <button @click="onSubmit">Submit</button>
-        <a :href="pdfUrl" v-if="downloaded" download="result.pdf">Download</a>
+        <div class="mb-10 max-w-[25vw]">
+            <UploadPDFButton @pdfChanged="updateFile" />
+        </div>
+
+        <div>
+            <p>Enter page index to swap (start from 0)</p>
+            <div class="flex flex-row justify-end mt-3 items-center">
+                <label for="A">First Index</label>
+                <input class="px-5 ml-5 w-32 h-10 text-white rounded-md dark:bg-gray-700" type="number" name="A"
+                    v-model="indexA" @input="onInputChange" />
+            </div>
+            <div class="flex flex-row justify-end mt-3 items-center">
+                <label for="B">Second Index</label>
+                <input class="px-5 ml-5 w-32 h-10 text-white rounded-md dark:bg-gray-700" type="number" name="B"
+                    v-model="indexB" @input="onInputChange" />
+            </div>
+        </div>
+        
+        <button @click="onSubmit"
+            class="mt-10 px-10 py-4 bg-gray-700 text-white font-bold text-xs leading-tight uppercase rounded-xl shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out">
+            Submit </button>
+        <div class="mt-5">
+            <a :href="pdfUrl" v-if="downloaded" download="result.pdf" class="px-10 py-4 bg-gray-700 text-white font-bold text-xs leading-tight uppercase rounded-xl shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out">Download</a>
+        </div>
+
+        
+
         <ErrorNotification :activated="isNotificationVisible" @onClose="isNotificationVisible = false" />
     </div>
 </template>
