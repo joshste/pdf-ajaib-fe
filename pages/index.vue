4b619dbd3b0e3182c43407bc0069625cac3633c3
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2022-12-10 14:30:45 +0700

    revised index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,6 @@
         <div class="h-screen flex justify-center items-center">
           <img src="~/assets/Logo.png" />
         </div>
-        <div class="h-screen flex justify-center items-center">
-          <NuxtLink to="/addtext">Add Text</NuxtLink>
-        </div>
     </div>
 </template>
    
