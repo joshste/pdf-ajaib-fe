0c771ce0071a8210b70851a2a758c360362daf73
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2022-12-10 20:11:08 +0700

    Fix minor bug

--- components/TextForm.vue
+++ components/TextForm.vue
@@ -43,10 +43,9 @@
             <div class="container flex flex-wrap items-left mx-auto flex space-x-10">
                 <button @click="onSubmit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
             <a :href="pdfUrl" v-if="downloaded" download="result.pdf" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Download</a>
-            <ErrorNotification :activated="isNotificationVisible" @onClose="isNotificationVisible = false" />
-        
             </div>
         </div>
+        <ErrorNotification :activated="isNotificationVisible" @onClose="isNotificationVisible = false" />
     </div>
 </template>
 
